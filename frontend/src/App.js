d3c705f32c706624d6855a6c16f4493adfdd6f76
namnguyen3019 <nam.nguy3019@gmail.com>
2020-10-12 12:04:26 -0500

    product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import HomeSreeen from './screens/HomeScreen';
 function App() {
     return (
         <>
@@ -10,6 +10,7 @@ function App() {
             <main className="py-3">
                 <Container>
                     <h1>Welcome to Proshop</h1>
+                    <HomeSreeen />
                 </Container>
             </main>
             <Footer />
